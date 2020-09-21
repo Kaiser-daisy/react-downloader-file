3eb5a089b2fbd7f69f03d78cb81f84226c8ea02c
Kaiser-daisy <625551001@qq.com>
2020-09-21 09:57:43 +0800

    merge

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # react-download-file
-react  file stream download
+react file stream download
+
+## Install
+```
+$ yarn add react-download-file
+$ npm install react-download-file
+```
+## Example
+```
+import React from 'react';
+import DownLoadFile,{DownloadFileProps} from 'react-download-file';
+
+const ExampleComponent:React.FC<DownloadFileProps> = props =>{
+    return (
+        <DownLoadFile
+            url="/user/info"
+            headerParams={{
+                method:'POST',
+                credentials:'include',
+            }}
+        >
+         <Button>下载</Button>
+        </DownLoadFile>
+   )
+}
+export default ExampleComponent;
+```
+## License
+*react-download-file* available under the MIT License
+
+## Keywords
+
+[react](https://www.npmjs.com/search?q=keywords:react) [fetch](https://www.npmjs.com/search?q=keywords:fetch)
+
+
+
+
+
---
+++ index.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import {fetch} from "umi-request";
+
+interface HeaderParamsProps{
+    method: 'POST' | 'GET'
+    credentials?:'omit' | 'same-origin' | 'include'
+}
+
+export interface DownloadFileProps {
+   url:string
+   headerParams:HeaderParamsProps
+   children:React.ReactNode
+}
+
+const DownLoadFile:React.FC<DownloadFileProps> = props =>{
+    const {url,headerParams,children} = props;
+    const {method,credentials,...rest} = headerParams;
+    const reg = new RegExp('"', 'g');
+    const handleClick = async () =>{
+        const response = await fetch(url,{
+            method,
+            headers:new Headers({
+                Accept:'application/json',
+                credentials,
+                ...rest,
+            }),
+            responseType:'blob',
+        });
+        if(response){
+            response.blob().then(blob =>{
+                const tag = document.createElement('a');
+                const fileUrl = window.URL.createObjectURL(blob);
+                const file = response.headers
+                    .get('Content-Disposition')
+                    .split('filename=')[1]
+                    .replace(reg,'');
+                const fileName = file.split('.');
+                tag.href = fileUrl;
+                tag.download = `${decodeURI(fileName[0])}.${fileName[1]}`;
+                tag.style.display = 'none';
+                document.body.append(tag);
+                tag.click();
+                window.URL.revokeObjectURL(fileUrl);
+                document.body.removeChild(tag);
+            })
+                .catch(() =>{
+                    console.log('文件下载失败');
+                })
+        }
+    };
+    return (<div onClick={handleClick}>{children}</div>)
+};
+export default DownLoadFile;
